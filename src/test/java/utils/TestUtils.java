d2bcdae18832adc3cd3d7a203df516a258841dc7
Asier <angel.sierra0507@gmail.com>
2024-12-31 20:24:52 -0500

    update the utils class for the implementation

--- src/test/java/utils/TestUtils.java
+++ src/test/java/utils/TestUtils.java
@@ -1,12 +1,7 @@
 package utils;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.JavascriptExecutor;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,46 +10,30 @@ import java.io.IOException;
 import java.time.Duration;
 
 public class TestUtils {
-
     private static WebDriver driver;
-
-
+    private final WebDriverWait wait;
 
     public TestUtils(WebDriver driver) {
-        this.driver = driver;
+        TestUtils.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
-
 
     public void highlightElement(WebElement element) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
+        String originalStyle = element.getAttribute("style");
         js.executeScript("arguments[0].style.border='3px solid blue';", element);
-    }
-
-
-    public String takeScreenshot(String screenshotName) throws IOException {
-        TakesScreenshot screenshotDriver = (TakesScreenshot) driver;
-        File srcFile = screenshotDriver.getScreenshotAs(OutputType.FILE);
-        String destination = "target/screenshots/" + screenshotName + ".png";
-        FileUtils.copyFile(srcFile, new File(destination));
-        return destination;
-    }
-
-
-    public void attachScreenshotToReport(String screenshotPath) {
-        String screenshotTag = "<img src='file://" + screenshotPath + "' width='600' height='400'/>";
-    }
-
-
-    public void highlightAndScreenshot(WebElement element, String screenshotName) throws IOException {
-        highlightElement(element);
-        String screenshotPath = takeScreenshot(screenshotName);
-        attachScreenshotToReport(screenshotPath);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        js.executeScript("arguments[0].setAttribute('style', '" + originalStyle + "');", element);
     }
 
     public WebElement waitForElement(By locator, int timeoutInSeconds) {
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
+
 
 }
